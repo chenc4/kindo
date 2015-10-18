c05a57de1994f88e8539c827665b602d5fc7ecfb
Henson <shenghe@users.noreply.github.com>
2015-10-18 15:16:02 +0800

    move hub to kindo-hub

--- hub/kindo-hub-infrastructure/src/main/java/io/kindo/hub/infrastructure/iface/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package io.kindo.hub.infrastructure.iface;
-
-import io.kindo.hub.infrastructure.po.Account;
-import org.springframework.data.repository.CrudRepository;
-
-public interface AccountRepository extends CrudRepository<Account, Long> {
-    Account findOneByUsername(String username);
-}
